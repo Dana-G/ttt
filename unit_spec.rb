10307ba6c05598b95d33f0cf830a891cc02d8976
Dana Giordano <dgiordano@weblinc.com>
2015-06-17 18:59:41 -0400

    game plan

---
+++ unit_spec.rb
@@ -0,0 +1,12 @@
+# make an empty board with 3 x 3 hardcoded
+# makes user pick if X or O
+# always starts with player 1
+# input validation: is the user giving us legal input value of letter symbol on the board
+# input validation: is space empty via nil or null
+# input validation: no numbers, length only 1 (sarahs grad school solution)
+# (just check for what you do want, not what you don't want)
+# track whats empty and use for legal input checker
+# input & show input on board
+# check if placement is winning combination
+# if not winning move, player 2 goes, if yes start new game or quit
+
